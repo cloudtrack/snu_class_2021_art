6835daa89f7b45f6bb7f4ed7362bf9775dce036f
matchy233 <matchy233@gmail.com>
2021-10-26 17:00:29 +0900

    AAAD-37 Refine file structure

--- src/pages/FeedPage/Feed.tsx
+++ src/pages/FeedPage/Feed.tsx
@@ -1,25 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Class.css';
+import ExploreContainer from '../../components/ExploreContainer';
+import './Feed.css';
 
-const ClassView: React.FC = () => {
+const FeedView: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default ClassView;
+export default FeedView;
