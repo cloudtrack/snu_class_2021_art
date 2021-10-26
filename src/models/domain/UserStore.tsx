2940dca74d3f22bf41384224068e3dad5eb956e9
matchy233 <matchy233@gmail.com>
2021-10-27 02:24:18 +0900

    Clean up user store structure

--- src/models/domain/UserStore.tsx
+++ src/models/domain/UserStore.tsx
@@ -5,87 +5,51 @@ import awsconfig from "../../aws-exports";
 Amplify.configure(awsconfig);
 
 class UserStore {
+  private rootStore
+  user: any | null = null
+  isConfirmed: boolean = false
+  isLoggedIn: boolean = false
 
-  private rootStore;
-
-  user : any
-
-  constructor(rootStore: any) {
+  constructor(rootStore : any) {
     this.rootStore = rootStore
     makeAutoObservable(this)
   }
 
-
-  async signUp(username: string, password:string) {
+  async signUp(username: string, password: string) {
     console.log("yooooo signing up userstore")
-      await Auth.signUp(username, password)
-          .then((user) => {
-            this.user = user
-            console.log(user)
-          })
-          .catch((error) => {
-            console.log('error signing up:', error)
-          });
+    await Auth.signUp(username, password)
+      .then((user) => {
+        this.rootStore.userStore.user = user
+        this.rootStore.userStore.isConfirmed = user.userConfirmed
+        console.log(user)
+      })
+      .catch((e) => {
+        console.log('error signing up:', e)
+      });
   }
 
   async signIn(username: string, password: string) {
-    await Auth.signIn(username, password).then(
-      user => {
-        if (user.challengeName === 'NEW_PASSWORD_REQUIRED') {
-          Auth.completeNewPassword(
-            user,               // the Cognito User Object
-            password,       // the new password
-          ).then(user => {
-            // at this time the user is logged in if no MFA required
-            this.user = user
-            console.log(user);
-          }).catch(e => {
-            console.log(e);
-          });
-        }
-      }).catch(e => {
+    await Auth.signIn(username, password)
+      .then(user => {
+        this.rootStore.userStore.user = user
+        this.rootStore.userStore.isLoggedIn = true
+        console.log(user)
+      })
+      .catch(e => {
         console.log(e);
       })
   }
+
+  async confirmAuthentication(username: string, password: string, code: string) {
+    try {
+      await Auth.confirmSignUp(username, code)
+      this.rootStore.userStore.isConfirmed = true
+      await Auth.signIn(username, password).then()
+      this.rootStore.userStore.isLoggedIn = true
+    } catch (e) {
+      console.log(e)
+    }
+  }
 }
 
-
-
 export default UserStore;
-
-
-// async function confirmSignUp() {
-//     try {
-//         await Auth.confirmSignUp(username, code);
-//     } catch (error) {
-//         console.log('error confirming sign up', error);
-//     }
-// }
-
-// async function signIn() {
-//     try {
-//         const user = await Auth.signIn(username, password);
-//     } catch (error) {
-//         console.log('error signing in', error);
-//     }
-// }
-
-// async function signOut() {
-//     try {
-//         await Auth.signOut();
-//     } catch (error) {
-//         console.log('error signing out: ', error);
-//     }
-// }
-// class RegisterModel {
-
-//     async signIn(params : any) {
-//         try {
-//             const user = await Auth.signIn(params);
-//             return user;
-//         } catch (error) {
-//             console.log('error signing in', error);
-//         }
-//     }
-
-// }
