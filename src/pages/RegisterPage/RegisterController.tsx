1e513d3620a4336dbbe00a8533fc449923b26ed8
matchy233 <matchy233@gmail.com>
2021-10-27 02:24:48 +0900

    AAAD-58 Implement registration page

--- src/pages/RegisterPage/RegisterController.tsx
+++ src/pages/RegisterPage/RegisterController.tsx
@@ -1,34 +1,45 @@
 import React from "react";
+import { useHistory } from "react-router";
 import UserStore from "../../models/domain/UserStore";
 import RegisterView from "./RegisterView";
 
 const RegisterController: React.FC<{ userStore: UserStore }> = (props) => {
   const userStore = props.userStore;
   const passwordRegex: RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+
+  const history = useHistory()
   const isUsernameValid = (username: string): boolean => {
-    return typeof username !== "undefined" &&
-      username !== null &&
+    return username !== null &&
       username !== "" &&
       username.includes("@") &&
       username.substr(username.indexOf("@")) !== ""
   }
 
   const isPasswordValid = (password: string): boolean => {
-    return typeof password !== "undefined" &&
-      password !== null &&
+    return password !== null &&
       password !== "" &&
       password.match(passwordRegex) !== null;
   }
 
-  const onSubmit = (username: string, password: string): void => {
+  const onSignUp = (username: string, password: string): void => {
     userStore.signUp(username, password)
+    history.push("/register")
+  }
+
+  const onConfirm = (username: string, password: string, code : string ) => {
+    userStore.confirmAuthentication(username, password, code)
+    if (userStore.isLoggedIn) {
+      history.push("/")
+    }
   }
 
   return (
     <RegisterView
+      user = {userStore.user}
       isUsernameValid={isUsernameValid}
       isPasswordValid={isPasswordValid}
-      onSubmit={onSubmit} />
+      onSignUp={onSignUp}
+      onConfirm={onConfirm} />
   )
 }
 
--- src/pages/RegisterPage/RegisterView.tsx
+++ src/pages/RegisterPage/RegisterView.tsx
@@ -1,18 +1,24 @@
 import { IonContent, IonPage, IonLabel, IonInput, IonItem, IonList, IonImg, IonButton } from "@ionic/react";
 import React, { useState } from "react";
+import {observer} from 'mobx-react'
 
 
 const RegisterView: React.FC<{
+  user: any | null,
   isUsernameValid: any,
   isPasswordValid: any,
-  onSubmit: any
-}> = (props) => {
+  onSignUp: any,
+  onConfirm: any
+}> = observer((props) => {
 
-  const [username, setUsername] = useState<string>();
-  const [password, setPassword] = useState<string>();
+  // const user = props.user
 
-  return (
-    <IonPage>
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [code, setCode] = useState<string>("");
+
+  const renderSignUp = (props : any) => {
+    return (
       <IonContent>
         <IonList>
           <IonImg
@@ -23,47 +29,67 @@ const RegisterView: React.FC<{
             src={"assets/icon/icon.png"}
           ></IonImg>
           <IonItem>
-            <IonLabel
-              position="floating"
-            >Email</IonLabel>
+            <IonLabel position="floating">Email</IonLabel>
             <IonInput
               value={username}
               placeholder="Enter Input"
-              onIonChange={e => setUsername(
-                e.detail.value!
-              )}
+              onIonChange={e => setUsername(e.detail.value!)}
               clearInput
             ></IonInput>
           </IonItem>
           <IonItem>
-            <IonLabel
-              position="floating"
-            >Password</IonLabel>
+            <IonLabel position="floating">Password</IonLabel>
             <IonInput
+            type="password"
               value={password}
-              placeholder="Enter Input"
-              onIonChange={e => setPassword(
-                e.detail.value!
-              )}
+              placeholder="Enter Password"
+              onIonChange={e => setPassword(e.detail.value!)}
               clearInput
             ></IonInput>
           </IonItem>
           <IonButton
-            style={{
-              margin: "50px 50px 50px 50px"
-            }}
+            style={{ margin: "50px 50px 50px 50px" }}
             expand="block"
             type="submit"
             onClick={() => {
               if (props.isUsernameValid(username) && props.isPasswordValid(password)) {
-                props.onSubmit(username, password)
+                props.onSignUp(username, password)
               }
             }}
           >Sign Up</IonButton>
         </IonList>
       </IonContent>
+    )
+  }
+  const renderConfirm = (props : any) => {
+    return (
+      <IonContent>
+        <IonList>
+          <IonItem>
+            <IonLabel position="floating">Confirmation Code</IonLabel>
+            <IonInput value={code}
+              placeholder="Enter Input"
+              onIonChange={e => setCode(e.detail.value!)}
+              clearInput
+            ></IonInput>
+          </IonItem>
+          <IonButton
+            style={{ margin: "50px 50px 50px 50px" }}
+            expand="block"
+            type="submit"
+            onClick={(e) => {
+              props.onConfirm(username, password, code)
+            }}
+          >Confirm Code</IonButton>
+        </IonList>
+      </IonContent>
+    )
+  }
+  return (
+    <IonPage>
+      {props.user === null ? renderSignUp(props) : renderConfirm(props)}
     </IonPage>
-  );
-};
+  )
+})
 
 export default RegisterView;
