22c257e36bb0787a1e11ce1158a3f7197b0a6663
matchy233 <matchy233@gmail.com>
2021-10-28 03:50:50 +0900

    AAAD-37 Implement login page

--- src/pages/LoginPage/LoginController.tsx
+++ src/pages/LoginPage/LoginController.tsx
@@ -1,17 +1,45 @@
 
 import Auth from "@aws-amplify/auth";
 import React from "react";
+import { useHistory } from "react-router";
 import UserStore from "../../models/domain/UserStore";
 import LoginView from "./LoginView";
 
 const LoginController: React.FC<{ userStore: UserStore }> = (props) => {
+  const history = useHistory()
+  const passwordRegex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
-  const onSubmit = (username: string, password: string) => {
-    Auth.signIn(username, password)
+  const isUsernameValid = (username: string): boolean => {
+    return username !== null &&
+      username !== "" &&
+      username.includes("@") &&
+      username.substr(username.indexOf("@")) !== ""
+  }
+
+  const isPasswordValid = (password: string): boolean => {
+    return password !== null &&
+      password !== "" &&
+      password.match(passwordRegex) !== null;
+  }
+
+  const onSubmit = async (username: string, password: string) => {
+    await Auth.signIn(username, password)
+      .then((user) => {
+        props.userStore.setUser(user)
+        props.userStore.setShouldConfirm(false)
+        props.userStore.setShouldLogIn(false)
+        props.userStore.setLoggedIn(true)
+        console.log(user)
+      })
+      .catch(e => { console.log(e) })
+    history.push("/")
   }
 
   return (
-    <LoginView onSubmit={onSubmit}/>
+    <LoginView
+    isUsernameValid={isUsernameValid}
+    isPasswordValid={isPasswordValid}
+    onSubmit={onSubmit} />
   )
 }
 
