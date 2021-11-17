1471d64517a576d02e4389e9b7efb8c0f125c9b5
matchy233 <matchy233@gmail.com>
2021-11-18 01:17:44 +0800

    refactor: navItem

--- src/components/NavList/index.tsx
+++ src/components/NavList/index.tsx
@@ -7,57 +7,69 @@ import {
   IonList,
   useIonAlert
 } from '@ionic/react';
-import { informationCircleOutline, logOut } from 'ionicons/icons';
 import { useState } from 'react';
+import { informationCircleOutline, logOut } from 'ionicons/icons';
 import './index.css';
 
 interface INavListProps {
   logout: () => void;
 }
+interface NavItem {
+  onClick: () => void;
+  label: string;
+  icon: string;
+}
 
 export const NavList: React.FC<INavListProps> = ({ logout }) => {
   const [showLogoutAlert, setShowLogoutAlert] = useState(false);
   const [present] = useIonAlert();
+  const navItem: NavItem[] = [
+    {
+      onClick: () => setShowLogoutAlert(true),
+      label: 'Log Out',
+      icon: logOut,
+    },
+    {
+      onClick: () => {
+        present('Art Class Application');
+      },
+      label: 'About',
+      icon: informationCircleOutline,
+    },
+  ];
   return (
     <IonCard>
       <IonAlert
-          isOpen={showLogoutAlert}
-          onDidDismiss={() => setShowLogoutAlert(false)}
-          // cssClass='my-custom-class'
-          header={'Log out'}
-          message={'Are you sure you want to log out?'}
-          buttons={[
-            {
-              text: 'Cancel',
-              role: 'cancel',
-              cssClass: 'secondary',
-              handler: () => {
-                console.log('Confirm Cancel');
-              }
-            },
-            {
-              text: 'Ok',
-              handler: () => {
-                console.log('Confirm Ok');
-                logout();
-              }
+        isOpen={showLogoutAlert}
+        onDidDismiss={() => setShowLogoutAlert(false)}
+        // cssClass='my-custom-class'
+        header={'Log out'}
+        message={'Are you sure you want to log out?'}
+        buttons={[
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
             }
-          ]}
-        />
+          },
+          {
+            text: 'Ok',
+            handler: () => {
+              console.log('Confirm Ok');
+              logout();
+            }
+          }
+        ]}
+      />
       <IonList>
-        <IonItem onClick={() => setShowLogoutAlert(true)} button>
-          <IonIcon slot={'start'} icon={logOut} color="primary" />
-          <IonLabel>Log Out</IonLabel>
-        </IonItem>
-        <IonItem
-          button
-          onClick={() => {
-            present('Art Class Application');
-          }}
-        >
-          <IonIcon slot={'start'} icon={informationCircleOutline} color="warning" />
-          <IonLabel>About</IonLabel>
-        </IonItem>
+        {navItem.map(({ onClick, icon, label }, index) => (
+          <IonItem key={index} onClick={onClick} button>
+            <IonIcon slot={'start'} icon={icon} color="primary" />
+            <IonLabel>{label}</IonLabel>
+          </IonItem>
+        ))}
       </IonList>
     </IonCard>
   );
