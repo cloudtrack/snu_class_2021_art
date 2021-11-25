b3bb19f705e72692ab0a1dceab74d5a2cc8d2d8b
matchy233 <matchy233@gmail.com>
2021-11-25 21:04:32 +0800

    feat: add class item

--- src/components/ClassItem/index.tsx
+++ src/components/ClassItem/index.tsx
@@ -1,24 +1,42 @@
-import { IonGrid, IonRow, IonAvatar, IonSkeletonText, IonCard, IonCardHeader, IonCardContent, IonIcon, IonText } from "@ionic/react";
-import { bookmark } from "ionicons/icons";
+import {
+  IonGrid,
+  IonRow,
+  IonAvatar,
+  IonSkeletonText,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonIcon,
+  IonText
+} from '@ionic/react';
+import { bookmark } from 'ionicons/icons';
+import { Teacher } from '../../models';
+import { avatarImageFromEmail } from '../../utils';
+import './index.css';
 
-interface IClassItemProps {}
+export interface IClassItemProps {
+  classname: string;
+  teacher: Teacher;
+}
 
-export const ClassItem: React.FC<IClassItemProps> = () => {
-  return (
-    <IonCard>
-      <IonCardHeader>
-        {/* Show class name and professor name and notif dot */}
-        <IonGrid>
-          <IonRow className="ion-align-itmes-center">
-            <IonIcon icon={bookmark} size="large"/>
-            <IonText>classname</IonText>
-          </IonRow>
-        </IonGrid>
-      </IonCardHeader>
-      <IonCardContent>
-        {/* Show class description */}
-        <IonSkeletonText animated style={{ width: "100%" }} />
-      </IonCardContent>
-    </IonCard>
-  );
-};
+export const ClassItem: React.FC<IClassItemProps> = ({ classname, teacher }) => (
+  <IonCard>
+    <IonCardHeader>
+      {/* Show class name and professor name and notif dot */}
+      <IonGrid>
+        <IonRow className="ion-align-items-center">
+          <IonIcon icon={bookmark} />
+          <IonText class="ion-margin-start">{classname}</IonText>
+          <IonAvatar class="ion-margin-start teacher-avatar-image">
+            <img src={avatarImageFromEmail(teacher.email!)} alt={teacher.name} />
+          </IonAvatar>
+          <IonText class="ion-margin-start">{teacher.name}</IonText>
+        </IonRow>
+      </IonGrid>
+    </IonCardHeader>
+    <IonCardContent>
+      {/* Show class description */}
+      <IonSkeletonText animated style={{ width: '100%' }} />
+    </IonCardContent>
+  </IonCard>
+);
--- src/pages/ClassPage/ClassView.tsx
+++ src/pages/ClassPage/ClassView.tsx
@@ -1,5 +1,15 @@
-import { IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { search } from 'ionicons/icons';
+import {
+  IonContent,
+  IonFab,
+  IonFabButton,
+  IonFabList,
+  IonHeader,
+  IonIcon,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import { add, camera, cloudUpload, search } from 'ionicons/icons';
 import { StudentClass } from '../../components/StudentClass';
 import { UserDataType } from '../../stores/UserStore';
 import './Class.css';
@@ -8,29 +18,24 @@ interface IClassViewProps {
   userData: UserDataType;
 }
 
-const ClassView: React.FC<IClassViewProps> = ({ userData }) => {
-  return (
-    <IonPage>
-      <IonHeader collapse="condense">
-        <IonToolbar>
-          <IonTitle size="large">Class</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        {/* <ExploreContainer name="Tab 2 page" /> */}
-        {
-          userData?.role === "student" ?
-            <StudentClass userData={userData} /> :
-            <></> // TODO: show class page for teacher
-        }
-      </IonContent>
-      <IonFab vertical="bottom" horizontal="end" slot="fixed">
-        <IonFabButton color="primary">
-          <IonIcon icon={search} />
-        </IonFabButton>
-      </IonFab>
-    </IonPage>
-  );
-}
+const ClassView: React.FC<IClassViewProps> = ({ userData }) => (
+  <IonPage>
+    <IonHeader collapse="condense">
+      <IonToolbar>
+        <IonTitle size="large">Class</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent fullscreen>
+      {
+        userData?.role === 'student' ? <StudentClass userData={userData} /> : <></> // TODO: show class page for teacher
+      }
+    </IonContent>
+    <IonFab vertical="bottom" horizontal="end" slot="fixed">
+      <IonFabButton color="primary">
+        <IonIcon icon={search} />
+      </IonFabButton>
+    </IonFab>
+  </IonPage>
+);
 
 export default ClassView;
