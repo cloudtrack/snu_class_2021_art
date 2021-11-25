a1011799f03db22e9b37fe21483d16968f378668
matchy233 <matchy233@gmail.com>
2021-11-25 21:04:32 +0800

    feat: add class item

--- src/components/StudentClass/index.tsx
+++ src/components/StudentClass/index.tsx
@@ -1,8 +1,9 @@
-import { RefresherEventDetail } from "@ionic/core";
-import { IonContent, IonRefresher, IonRefresherContent } from "@ionic/react";
-import { ClassItem } from "../ClassItem";
-import { UserDataType } from "../../stores/UserStore";
-
+import { RefresherEventDetail } from '@ionic/core';
+import { IonContent, IonRefresher, IonRefresherContent, IonSearchbar, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
+import { UserDataType } from '../../stores/UserStore';
+import { ClassItem, IClassItemProps } from '../ClassItem';
+import Faker from 'faker';
 interface IStudentClassProps {
   userData: UserDataType;
 }
@@ -18,18 +19,38 @@ function doRefresh(event: CustomEvent<RefresherEventDetail>) {
 
 export const StudentClass: React.FC<IStudentClassProps> = ({ userData }) => {
   // show the classes student is taking in card
+  const [searchText, setSearchText] = useState('');
 
+  const fakeClassList: IClassItemProps[] = [];
+  for (let i = 0; i < 2; i++) {
+    fakeClassList.push({
+      classname: Faker.name.firstName(),
+      teacher: {
+        id: Faker.random.uuid(),
+        name: Faker.name.findName(),
+        email: Faker.internet.email(),
+      },
+    });
+  }
   return (
     <>
       <IonRefresher slot="fixed" onIonRefresh={doRefresh} pullFactor={0.5} pullMin={100} pullMax={200}>
-        <IonRefresherContent>
-        </IonRefresherContent>
+        <IonRefresherContent />
       </IonRefresher>
+      <IonToolbar className="ion-margin-top">
+        <IonSearchbar
+          showClearButton="focus"
+          showCancelButton="focus"
+          placeholder="Search art class"
+          value={searchText}
+          onIonChange={e => setSearchText(e.detail.value!)}
+        />
+      </IonToolbar>
       <IonContent>
-        {new Array(10).fill(null).map(() => (
-          <ClassItem />
+        {fakeClassList.map((clazz: IClassItemProps) => (
+          <ClassItem {...clazz} />
         ))}
       </IonContent>
     </>
   );
-}
+};
