b9f00893f02133bd5aac7c7793cbfe48899db7bb
matchy233 <matchy233@gmail.com>
2021-12-06 18:33:37 +0900

    refactor: move fab to studentClass page

--- src/components/StudentClass/ClassSearchModal.tsx
+++ src/components/StudentClass/ClassSearchModal.tsx
@@ -1,15 +1,17 @@
-import { IonButton, IonButtons, IonHeader, IonIcon, IonModal, IonToolbar } from "@ionic/react";
+import { IonButton, IonButtons, IonContent, IonFooter, IonHeader, IonIcon, IonModal, IonSearchbar, IonToolbar } from "@ionic/react";
 import { arrowBack } from "ionicons/icons";
 import { observer } from "mobx-react";
+import { useEffect, useState } from "react";
 
 interface IClassSearchModalProps {
-  showModal : boolean;
+  showModal: boolean;
   onDidDismiss: () => void;
 };
 
 
-const ClassSearchModal : React.FC<IClassSearchModalProps> = (props) => {
+const ClassSearchModal: React.FC<IClassSearchModalProps> = (props) => {
   const { showModal, onDidDismiss } = props;
+  const [searchText, setSearchText] = useState('');
 
   return (
     <IonModal
@@ -25,6 +27,19 @@ const ClassSearchModal : React.FC<IClassSearchModalProps> = (props) => {
           </IonButtons>
         </IonToolbar>
       </IonHeader>
+      <IonContent>
+        <IonSearchbar
+          value={searchText}
+          onIonChange={
+            (e) => {
+              setSearchText(e.detail.value!)
+            }} />
+      </IonContent>
+      <IonFooter>
+        <IonToolbar>
+          Search Text: {searchText ?? '(none)'}
+        </IonToolbar>
+      </IonFooter>
     </IonModal>
 
   );
