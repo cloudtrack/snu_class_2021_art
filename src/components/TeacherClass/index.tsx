39aeacb2c976617996cb2d9f8e15eded64456aad
matchy233 <matchy233@gmail.com>
2021-12-06 20:15:28 +0900

    feat: add fab for adding classes

--- src/components/TeacherClass/index.tsx
+++ src/components/TeacherClass/index.tsx
@@ -1,7 +1,31 @@
-export const TeacherClass = () => {
+import { IonFab, IonFabButton, IonIcon } from "@ionic/react";
+import { add } from "ionicons/icons";
+import { useState } from "react";
+import { UserDataType } from "../../stores/UserStore";
+
+interface ITeacherClassProps {
+  userData: UserDataType;
+}
+
+
+export const TeacherClass : React.FC<ITeacherClassProps> = ({userData}) => {
+  const [ isFabOpen, setIsFabOpen ] = useState(false);
   return (
-    <div>
-      <h1>Teacher Class</h1>
-    </div>
+    <>
+      {/* Implement add class component */}
+      <></>
+      <div>
+        <h1>Teacher Class</h1>
+      </div>
+      <IonFab vertical="bottom" horizontal="end" slot="fixed">
+        <IonFabButton color="primary">
+          <IonIcon
+            icon={add}
+            onClick={() => {
+              setIsFabOpen(true);
+            }} />
+        </IonFabButton>
+      </IonFab>
+    </>
   );
 };
