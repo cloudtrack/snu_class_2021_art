be2fa596c888c779a96fb2b5de063ac244e383c3
matchy233 <matchy233@gmail.com>
2021-12-16 06:29:39 +0900

    feat: imeplement add assignments

---
+++ src/stores/AssignmentStore.tsx
@@ -0,0 +1,86 @@
+import { DataStore } from "aws-amplify";
+import { action, autorun, makeObservable, observable } from "mobx";
+import { Assignment, Class } from "../models";
+import RootStore from "./RootStore";
+
+class AssignmentStore {
+  rootStore: RootStore;
+
+  assignments: Assignment[] = [];
+  assignmetnIDs: string[] = [];
+
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+    autorun(() => {
+      if (this.rootStore.userStore.userData !== null &&
+        this.rootStore.classStore.classes.length > 0) {
+        this.initialize();
+        console.log("AssignmentStore created");
+        console.log(this.assignments);
+      }
+    })
+
+    makeObservable(this, {
+      assignments: observable,
+      initialize: action,
+      addAssignment: action,
+      deleteAssignment: action,
+    });
+  }
+
+  initialize = async () => {
+    if (this.rootStore.classStore.classes.length > 0) {
+      for (const classItem of this.rootStore.classStore.classes) {
+        if (classItem.Assignments === undefined) {
+          const assignments = await DataStore.query(Assignment);
+          for (const assn of assignments) {
+            if (assn.classID === classItem.id) {
+              this._addAssignment(assn);
+              console.log(this.assignments)
+            }
+          }
+          await DataStore.save(Class.copyOf(
+            classItem, item => {
+              item.Assignments = [...this.assignments.filter(assn => assn.classID === classItem.id)];
+            }
+          ));
+        }
+      }
+    }
+  }
+
+  addAssignment = async (
+    classId: string,
+    description: string,
+    openTime: string,
+    dueDate: string,
+  ) => {
+    if (this.assignments.filter(
+      assn => assn.classID === classId).length > 0) {
+      const assn = new Assignment({
+        "description": description,
+        "openTime": openTime,
+        "deadline": dueDate,
+        "ArtWorks": [],
+        "classID": classId,
+      });
+      this._addAssignment(assn);
+      await DataStore.save(assn);
+    } else {
+      throw new Error("Class not found");
+    }
+  }
+
+  _addAssignment = (assn: Assignment) => {
+    if (this.assignmetnIDs.includes(assn.id) === false) {
+      this.assignmetnIDs.push(assn.id); // already dealt with this assignment
+      this.assignments.push(assn);
+    }
+  }
+
+  deleteAssignment = async (classId: string, assignmentId: string) => {
+
+  }
+}
+
+export default AssignmentStore;
--- src/stores/RootStore.tsx
+++ src/stores/RootStore.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import AssignmentStore from "./AssignmentStore";
 import ClassStore from "./ClassStore";
 import PictureStore from "./PictrueStore";
 import UserStore from "./UserStore"
@@ -7,10 +8,12 @@ class RootStore {
   userStore: UserStore;
   pictureStore: PictureStore;
   classStore : ClassStore;
+  assignmentStore: AssignmentStore;
   constructor() {
     this.userStore = new UserStore(this);
     this.pictureStore = new PictureStore(this);
     this.classStore = new ClassStore(this);
+    this.assignmentStore = new AssignmentStore(this);
   }
 }
 
