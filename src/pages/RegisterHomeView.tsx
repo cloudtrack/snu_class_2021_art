fb7e19f36bbe69ac627338af0789779a99d95104
matchy233 <matchy233@gmail.com>
2022-01-01 15:38:04 +0900

    feat: add home screen bgimage and font

--- src/pages/RegisterHomeView.tsx
+++ src/pages/RegisterHomeView.tsx
@@ -3,41 +3,70 @@ import { logoFacebook, logoGoogle, logoIonic } from "ionicons/icons";
 import "./RegisterPage/RegisterController"
 import "./RegisterHome.css"
 import { observer } from "mobx-react";
+import { Auth } from "aws-amplify";
+import { CognitoHostedUIIdentityProvider } from "@aws-amplify/auth/lib/types";
 
 const RegisterHomeView: React.FC = () => {
   return (
-    <IonPage>
+    <IonPage >
       <IonHeader >
         <IonToolbar >
           <IonTitle class="ion-text-center">
-            <IonIcon icon={logoIonic} />  {/* TODO: find a new icon */}
+            <IonIcon color="primary" icon={logoIonic} />
           </IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent >
+      <IonContent className="homescreen" style={{
+        "--background": "none",
+        "background-image": "url(/assets/background2.jpg)",
+        "background-position": "center top",
+        "background-repeat": "no-repeat",
+        "background-size": "cover",
+      }}>
         <div className="container">
-          <IonText >
-            <h1>Build your art skills in the cloud<br />
-              Demonstrate and share learning easily</h1>
+          <IonText color="light" style={{
+            "font-family": "millenia, serif",
+            "font-size": "3rem",
+            // "text-shadow": "2px 2px #000000",
+            "text-shadow": "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black"
+          }}>
+            Build your art skills in the cloud<br />
+            Demonstrate and share learning easily
           </IonText>
         </div>
         <div className="footer">
-          <IonButton color="danger" expand="block">
+          <IonButton color="danger" expand="block" onClick={
+            () => {
+              // amplify google federated signin
+              Auth.federatedSignIn({
+                provider: CognitoHostedUIIdentityProvider.Google,
+              }).then(credentials => {
+                console.log(credentials);
+              }).catch(err => console.log(err));
+            }}>
             <IonIcon slot="start" icon={logoGoogle} />
             Continue with Google
           </IonButton>
           <IonButton expand="block" color="tertiary">
-            <IonIcon slot="start" icon={logoFacebook} />
+            <IonIcon slot="start" icon={logoFacebook} onClick={
+              () => {
+
+              }
+            } />
             Continue with Facebook
           </IonButton>
-          <IonText color="medium" class="ion-text-center">
+          <IonText color="light" class="ion-text-center" style={{
+            "text-shadow": "1px 1px #0b0b0b"
+          }}>
             Or
           </IonText>
-          <IonButton fill="outline" expand="block" routerLink="/register" color="primary">
+          <IonButton fill="solid" expand="block" routerLink="/register" color="light">
             Create account
           </IonButton>
-          <IonText color="medium">
-            Have an account already? <IonRouterLink routerLink="/login">
+          <IonText color="light" style={{
+            "text-shadow": "1px 1px #0b0b0b"
+          }}>
+            Have an account already? <IonRouterLink color="secondary" routerLink="/login">
               Log in
             </IonRouterLink>
           </IonText>
